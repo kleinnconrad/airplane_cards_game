07217e64dbcb8ebefa18720c8b3579c875763490
Kleinnmaleins <johanna.kleinn@web.de>
2026-09-20 16:26:59 +0200

    Generate and configure PWA app icons and automate CI

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,19 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'favicon.svg',
-            sizes: '192x192 512x512 any',
-            type: 'image/svg+xml',
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
